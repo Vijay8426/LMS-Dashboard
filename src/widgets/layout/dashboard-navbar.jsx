bd1a6806e1c8bd96a00b949ecf88fffc175e8fa1
Vijay <vijay@kalimachine.Kali>
2025-08-19 23:21:14 +0530

    navbar fixed

--- src/widgets/layout/dashboard-navbar.jsx
+++ src/widgets/layout/dashboard-navbar.jsx
@@ -1,13 +1,30 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import {
   Navbar,
   Typography,
-  Input,
+  Button,
   IconButton,
   Breadcrumbs,
+  Input,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Avatar,
 } from "@material-tailwind/react";
-import { BellIcon, ClockIcon, CreditCardIcon } from "@heroicons/react/24/solid";
-import { useMaterialTailwindController, setOpenSidenav } from "@/context";
+import {
+  UserCircleIcon,
+  Cog6ToothIcon,
+  BellIcon,
+  ClockIcon,
+  CreditCardIcon,
+  Bars3Icon,
+} from "@heroicons/react/24/solid";
+import {
+  useMaterialTailwindController,
+  setOpenConfigurator,
+  setOpenSidenav,
+} from "@/context";
 
 export function DashboardNavbar() {
   const [controller, dispatch] = useMaterialTailwindController();
@@ -27,21 +44,26 @@ export function DashboardNavbar() {
       blurred={fixedNavbar}
     >
       <div className="flex flex-col-reverse justify-between gap-6 md:flex-row md:items-center">
-        {/* Left: Breadcrumbs */}
         <div className="capitalize">
           <Breadcrumbs
             className={`bg-transparent p-0 transition-all ${
               fixedNavbar ? "mt-1" : ""
             }`}
           >
+            <Link to={`/${layout}`}>
+              <Typography
+                variant="small"
+                color="blue-gray"
+                className="font-normal opacity-50 transition-all hover:text-blue-500 hover:opacity-100"
+              >
+                {layout}
+              </Typography>
+            </Link>
             <Typography
               variant="small"
               color="blue-gray"
-              className="font-normal opacity-50 transition-all hover:text-blue-500 hover:opacity-100"
+              className="font-normal"
             >
-              {layout}
-            </Typography>
-            <Typography variant="small" color="blue-gray" className="font-normal">
               {page}
             </Typography>
           </Breadcrumbs>
@@ -49,12 +71,25 @@ export function DashboardNavbar() {
             {page}
           </Typography>
         </div>
+        <div className="flex items-center">
+
+          <IconButton
+            variant="text"
+            color="blue-gray"
+            className="grid xl:hidden"
+            onClick={() => setOpenSidenav(dispatch, !openSidenav)}
+          >
+            <Bars3Icon strokeWidth={3} className="h-6 w-6 text-blue-gray-500" />
+          </IconButton>
 
 
+
+        </div>
       </div>
     </Navbar>
   );
 }
 
 DashboardNavbar.displayName = "/src/widgets/layout/dashboard-navbar.jsx";
+
 export default DashboardNavbar;
